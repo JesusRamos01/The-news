474e3cfa11f3e714a940091e588cd5b35fde6043
JesusRamos01 <jesus.ramosvalois@unicolombo.edu.co>
2025-09-02 22:05:49 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'the-news',
+  webDir: 'www'
+};
+
+export default config;
